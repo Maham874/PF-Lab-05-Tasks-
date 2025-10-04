613fe5a30d3b9afd2d66471cbac9d2cdcd90ff54
Maham Mustafa <mahammustafa2007@gmail.com>
2025-10-04 20:17:53 +0500

    Add files via upload

---
+++ lab 05...07.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main ()
+{
+	int a = 7;
+	int b = 15;
+	int c = 10;
+	
+	  if (a < b && a < c)
+	  {
+	  printf("a is smallest");
+      }
+	  else if (b < c)
+	  {
+	  	printf("b is smallest");
+	  }
+	  else 
+	  {
+	  	printf("c is smallest");
+	  }
+	  return 0;
+}
